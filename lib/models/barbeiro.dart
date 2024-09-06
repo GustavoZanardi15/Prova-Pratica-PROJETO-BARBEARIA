7b6762040de1125971867bc060c833e58b9a8dce
GustavoZanardi15 <ulianzanardig@gmail.com>
2024-09-06 16:27:01 -0300

    Criação de classes

---
+++ lib/models/barbeiro.dart
@@ -0,0 +1,11 @@
+class Barbeiro{
+
+ String nome;
+ int _id;
+
+ Barbeiro(this.nome, this._id );
+     
+
+
+ 
+}
---
+++ lib/models/serviço.dart
@@ -0,0 +1,9 @@
+class Servico{
+
+ String nome;
+ int _id;
+ int preco;
+
+Servico(this.nome, this._id, this.preco);
+
+}
