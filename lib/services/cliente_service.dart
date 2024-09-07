8b95d242d7489c29512ab0e2856cac5d8b6fb8b5
ZanardiGustavo <134883869+ZanardiGustavo@users.noreply.github.com>
2024-09-07 18:17:33 -0300

    CRUD na classe CLIENTE_SERVICE

---
+++ lib/services/cliente_service.dart
@@ -0,0 +1,25 @@
+import 'package:barbearia/models/cliente.dart';
+
+class ClienteService {
+
+  final List<Cliente> _clientes = [];
+
+  void adicionarCliente(Cliente cliente){
+    _clientes.add(cliente);
+  }
+
+  void updateCliente(int id , Cliente clienteUpdate){
+  final index = _clientes.indexWhere((cliente)=> cliente.id == id);
+  if(index != -1){
+    _clientes[index] = clienteUpdate;
+    }
+  }
+
+  void deleteCliente(int id){
+    _clientes.removeWhere((cliente)=> cliente.id == id);
+  }
+
+  List<Cliente>getClientes(){
+    return _clientes;
+  }
+}
